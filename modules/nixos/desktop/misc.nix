6a998cbbc3bbe6e3790ed3350dd1c40e354d7779
HPCesia <me@hpcesia.com>
2025-06-02 01:32:51 +0800

    feat(app): modify shell config

--- modules/nixos/desktop/misc.nix
+++ modules/nixos/desktop/misc.nix
@@ -8,7 +8,7 @@
   # add user's shell into /etc/shells
   environment.shells = with pkgs; [
     bashInteractive
-    pkgs.fish
+    fish
   ];
   # set user's default shell system-wide
   users.defaultUserShell = pkgs.bashInteractive;
