57658a5ba7cde84e57ae599d5ac4a800d22d8cd9
HPCesia <me@hpcesia.com>
2025-06-07 21:35:47 +0800

    feat(app): add more applications

--- home/base/tui/editors/packages.nix
+++ home/base/tui/editors/packages.nix
@@ -1,13 +1,65 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  pkgs-unstable,
+  ...
+}: {
   home.packages = with pkgs; [
     # === Data & Configuration Languages === #
     # -- Nix
     nil # Nix LSP
     alejandra # Nix Code Formatter
 
+    # -- Json Like
+    taplo # TOML LSP / formatter / validator
+    nodePackages.yaml-language-server
+    actionlint # GitHub Actions linter
+
     # -- Documents
     marksman # Markdown LSP
     tinymist # Typst LSP
     typstyle # Typst Formatter
+
+    # === General Purpose Languages === #
+    # -- C/C++
+    cmake
+    cmake-language-server
+    gnumake
+    checkmake
+    xmake
+    gcc
+    gdb
+    clang-tools
+    lldb
+
+    # -- Python
+    uv # Python package manager in rust
+    pixi # Python package manager in rust, supports conda
+    ruff # Python LSP and formatter
+    python313
+
+    #-- rust
+    pkgs-unstable.rustc
+    pkgs-unstable.rust-analyzer
+    pkgs-unstable.cargo # rust package manager
+    pkgs-unstable.rustfmt
+    pkgs-unstable.clippy # rust linter
+
+    # -- Lua
+    stylua
+    lua-language-server
+
+    #-- bash
+    nodePackages.bash-language-server
+    shellcheck
+    shfmt
+
+    # === Web Development === #
+    nodePackages.nodejs
+    pnpm
+    nodePackages.typescript
+    nodePackages.typescript-language-server
+    nodePackages.vscode-langservers-extracted # HTML/CSS/JSON/ESLint LSP extracted from vscode
+    nodePackages."@tailwindcss/language-server"
+    nodePackages."@astrojs/language-server"
   ];
 }
--- home/linux/gui/misc.nix
+++ home/linux/gui/misc.nix
@@ -1,13 +1,35 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  inherit (lib.lists) concatLists;
+
+  # Fix Electron IME bug
+  cherrystudio = pkgs.cherry-studio.override {
+    commandLineArgs = concatLists [
+      ["--ozone-platform-hint=auto"]
+      ["--enable-wayland-ime"]
+      ["--wayland-text-input-version=3"]
+    ];
+  };
+in {
   home.packages = with pkgs; [
     chromium
 
     thunderbird
 
+    # Video
+    obs-studio
+    kdePackages.kdenlive
+
     # Message
     telegram-desktop
     qq
     wechat-uos
+
+    # Misc
+    cherrystudio
   ];
 
   # allow fontconfig to discover fonts and configurations installed through home.packages
