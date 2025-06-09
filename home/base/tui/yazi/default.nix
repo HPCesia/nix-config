60e85d799ba33d4d829e311aff6af00928f40020
HPCesia <me@hpcesia.com>
2025-06-09 17:02:09 +0800

    feat(app): update Yazi config

---
+++ home/base/tui/yazi/default.nix
@@ -0,0 +1,37 @@
+{pkgs, ...}: {
+  catppuccin.yazi.enable = true;
+
+  programs.yazi = {
+    enable = true;
+    enableBashIntegration = true;
+    enableNushellIntegration = true;
+    plugins = {
+      git = pkgs.yaziPlugins.git;
+      starship = pkgs.yaziPlugins.starship;
+      auto-layout = ./plugins/auto-layout.yazi;
+    };
+    initLua = ''
+      require("git"):setup()
+      require("starship"):setup()
+      require("auto-layout").setup()
+    '';
+    settings = {
+      mgr = {
+        show_hidden = true;
+        sort_dir_first = true;
+      };
+      plugin.prepend_fetchers = [
+        {
+          id = "git";
+          name = "*";
+          run = "git";
+        }
+        {
+          id = "git";
+          name = "*/";
+          run = "git";
+        }
+      ];
+    };
+  };
+}
