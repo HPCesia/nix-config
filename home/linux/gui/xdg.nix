00306fd434cdf3e20a384f0475f94e43db3d429d
HPCesia <me@hpcesia.com>
2025-06-15 01:48:58 +0800

    feat(desktop): Update XDG user dirs

--- home/linux/gui/xdg.nix
+++ home/linux/gui/xdg.nix
@@ -17,6 +17,19 @@
     dataHome = "${config.home.homeDirectory}/.local/share";
     stateHome = "${config.home.homeDirectory}/.local/state";
 
+    userDirs = {
+      enable = true;
+      # Use English user directroy path
+      desktop = "${config.home.homeDirectory}/Desktop";
+      documents = "${config.home.homeDirectory}/Documents";
+      download = "${config.home.homeDirectory}/Download";
+      music = "${config.home.homeDirectory}/Music";
+      pictures = "${config.home.homeDirectory}/Pictures";
+      publicShare = "${config.home.homeDirectory}/Public";
+      templates = "${config.home.homeDirectory}/Templates";
+      videos = "${config.home.homeDirectory}/Videos";
+    };
+
     mimeApps = {
       enable = true;
       defaultApplications = let
