5757c77e56bb2b418aebb1c21a9c3ecb3c65dacc
HPCesia <me@hpcesia.com>
2025-07-03 01:00:12 +0800

    feat(app): update zellij config

--- home/base/tui/zellij.nix
+++ home/base/tui/zellij.nix
@@ -15,23 +15,23 @@ in {
   };
 
   # auto start zellij in nushell
-  programs.nushell.extraConfig = ''
-    # auto start zellij
-    # except when in VSCode or zellij itself
-    if (not ("ZELLIJ" in $env)) and (not ($env.TERM_PROGRAM? == "vscode")) {
-      if "ZELLIJ_AUTO_ATTACH" in $env and $env.ZELLIJ_AUTO_ATTACH == "true" {
-        ^zellij attach -c
-      } else {
-        ^zellij
-      }
+  # programs.nushell.extraConfig = ''
+  #   # auto start zellij
+  #   # except when in VSCode or zellij itself
+  #   if (not ("ZELLIJ" in $env)) and (not ($env.TERM_PROGRAM? == "vscode")) {
+  #     if "ZELLIJ_AUTO_ATTACH" in $env and $env.ZELLIJ_AUTO_ATTACH == "true" {
+  #       ^zellij attach -c
+  #     } else {
+  #       ^zellij
+  #     }
 
-      # Auto exit the shell session when zellij exit
-      $env.ZELLIJ_AUTO_EXIT = "false" # disable auto exit
-      if "ZELLIJ_AUTO_EXIT" in $env and $env.ZELLIJ_AUTO_EXIT == "true" {
-        exit
-      }
-    }
-  '';
+  #     # Auto exit the shell session when zellij exit
+  #     $env.ZELLIJ_AUTO_EXIT = "false" # disable auto exit
+  #     if "ZELLIJ_AUTO_EXIT" in $env and $env.ZELLIJ_AUTO_EXIT == "true" {
+  #       exit
+  #     }
+  #   }
+  # '';
 
   # only works in bash/zsh, not nushell
   home.shellAliases = shellAliases;
