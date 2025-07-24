ba34c9057546b2633c4731b143bbffab4764e6ff
HPCesia <me@hpcesia.com>
2025-07-25 00:37:04 +0800

    chore: disable nju nix mirror

--- modules/base/nix.nix
+++ modules/base/nix.nix
@@ -9,7 +9,7 @@
     substituters = [
       # cache mirror located in China
       # "https://mirrors.ustc.edu.cn/nix-channels/store"
-      "https://mirror.sjtu.edu.cn/nix-channels/store"
+      # "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
 
       "https://nix-community.cachix.org"
