1265ac88b9032422b82f842bccf2917649d49ff5
HPCesia <me@hpcesia.com>
2025-07-25 21:45:52 +0800

    fix: thunderbird catppuccin theme

--- home/linux/gui/thunderbird.nix
+++ home/linux/gui/thunderbird.nix
@@ -3,8 +3,13 @@
     enable = true;
     profiles.Default = {
       isDefault = true;
+      settings = {
+        "extensions.autoDisableScopes" = 0;
+      };
     };
   };
+
+  catppuccin.thunderbird.profile = "Default";
 
   accounts.email.accounts = {
     "${myvars.useremail}".thunderbird.enable = true;
