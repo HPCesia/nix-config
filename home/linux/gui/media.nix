3ae17d04113c626869d2b571c414edf5d43f3dbe
HPCesia <me@hpcesia.com>
2025-07-28 14:37:03 +0800

    fix(app): audacious desktop entry

--- home/linux/gui/media.nix
+++ home/linux/gui/media.nix
@@ -1,22 +1,14 @@
-{
-  pkgs,
-  pkgs-unstable,
-  lib,
-  ...
-}: let
-  audacious-wrapped = let
-    p = pkgs-unstable.audacious;
-  in
-    lib.hiPrio (
-      pkgs.runCommand "${p.name}-wrapped" {nativeBuildInputs = with pkgs; [makeWrapper];} ''
-        mkdir -p $out/bin
-        makeWrapper \
-          ${p}/bin/${p.meta.mainProgram or p.pname} \
-          $out/bin/${p.meta.mainProgram or p.pname} \
-          --set GTK_IM_MODULE wayland \
-          --set QT_IM_MODULE fcitx
-      ''
-    );
+{pkgs, ...}: let
+  audacious = (pkgs.audacious.override {withPlugins = true;}).overrideAttrs (
+    previousAttrs: {
+      qtWrapperArgs =
+        (previousAttrs.qtWrapperArgs or [])
+        ++ [
+          "--set GTK_IM_MODULE wayland"
+          "--set QT_IM_MODULE fcitx"
+        ];
+    }
+  );
 in {
   programs = {
     mpv = {
@@ -27,6 +19,6 @@ in {
   };
 
   home.packages = with pkgs; [
-    audacious-wrapped
+    audacious
   ];
 }
