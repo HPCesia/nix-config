0a2241c9edf38657f0c69bdfa4bc404bda987106
HPCesia <me@hpcesia.com>
2025-08-02 15:55:25 +0800

    feat: update lazygit config

--- home/base/tui/lazygit.nix
+++ home/base/tui/lazygit.nix
@@ -1,5 +1,11 @@
 {...}: {
   programs.lazygit = {
     enable = true;
+    settings = {
+      git.paging = {
+        colorArg = "always";
+        pager = "delta --dark --paging=never";
+      };
+    };
   };
 }
