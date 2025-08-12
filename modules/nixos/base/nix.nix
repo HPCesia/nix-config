2c4ae1c3433fbea792314ec7efda738625857ffa
HPCesia <me@hpcesia.com>
2025-08-12 16:27:31 +0800

    feat: disable nixos tools

--- modules/nixos/base/nix.nix
+++ modules/nixos/base/nix.nix
@@ -12,6 +12,16 @@
     clean.extraArgs = "--keep-since 4d --keep 3";
   };
 
+  system.tools = {
+    # disable useless tools
+    nixos-build-vms.enable = false;
+    nixos-enter.enable = false;
+    nixos-generate-config.enable = false;
+    nixos-install.enable = false;
+    nixos-option.enable = false;
+    nixos-rebuild.enable = false; # No needed, use `nh os` instead.
+  };
+
   # Manual optimise storage: nix-store --optimise
   # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
   nix.settings.auto-optimise-store = true;
