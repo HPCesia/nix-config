639c86df0e033be5b2a46658c5f92a50f0ec59bd
HPCesia <me@hpcesia.com>
2025-08-14 15:45:58 +0800

    fix: vscode setup script

--- home/linux/gui/vscode/default.nix
+++ home/linux/gui/vscode/default.nix
@@ -44,6 +44,13 @@ in {
       ExecStart = lib.getExe (pkgs.writeShellApplication {
         name = "vscode-setup";
         text = ''
+          if [ -d "${userDataDir}/User/globalStorage" ]; then
+            mv "${userDataDir}/User/globalStorage" /tmp/vscode-globalStorage-$$
+          fi
+          if [ -d "${userDataDir}/User/workspaceStorage" ]; then
+            mv "${userDataDir}/User/workspaceStorage" /tmp/vscode-workspaceStorage-$$
+          fi
+
           rm -rf "${userDataDir}/User"
           rm -rf "${extensionsDir}"
 
@@ -54,6 +61,13 @@ in {
             "${homeDir}/.config/Code/User" "${userDataDir}/User"
           cp -r --dereference --no-preserve=mode,ownership \
             "${homeDir}/.vscode/extensions/." "${extensionsDir}"
+
+          if [ -d /tmp/vscode-globalStorage-$$ ]; then
+            mv /tmp/vscode-globalStorage-$$ "${userDataDir}/User/globalStorage"
+          fi
+          if [ -d /tmp/vscode-workspaceStorage-$$ ]; then
+            mv /tmp/vscode-workspaceStorage-$$ "${userDataDir}/User/workspaceStorage"
+          fi
         '';
       });
     };
